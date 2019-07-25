453696b06f10e3d2e3639c17e6c445a98fb909c7
mjmailliard <mjmailliard@gmail.com>
2019-07-25 08:31:32 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { PersonsComponent } from './persons/persons.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PersonsComponent
   ],
   imports: [
     BrowserModule
